db6fc7e6bbc9c5a7bf411742050e3037e9629999
Bilal Javaid <sb100513@gmail.com>
2023-09-27 21:10:45 +0500

    next

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
 
           <div className='advisePara'>
             <p className='advise'>
-           
+
               {
                 advise[myCounter - 1]
               }
